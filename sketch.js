85eb8e0dabd5a01a6682b57ce52fc8e32b5070e5
maria luiza <125272614+marialuizadk@users.noreply.github.com>
2023-11-13 11:47:42 -0300

    22

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos= document.getElementById('segundos');
+
+const relogio = setInterval (function time)) {
+    let dateToday= new dateToday();
+    let hr = dateToday = dateToday.getHours();
+    let min= dateToday = dateToday.getMinutes();
+    let seg= dateToday = dateToday.getMilleseconds();
+
+    if(hr <10) hr = '0' + hr;
+    if(min <10) min = '0' + min;
+    if(seg <10) seg= '0' + seg;
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = seg
+
+}
+]
