9be7ff22648e04ffd264ffc1434ad062a3497215
maria luiza <125272614+marialuizadk@users.noreply.github.com>
2023-11-20 07:57:03 -0300

    999

--- sketch.js
+++ sketch.js
@@ -9,7 +9,9 @@ const relogio = setInterval (function time)) {
     let seg= dateToday = dateToday.getMilleseconds();
 
     if(hr <10) hr = '0' + hr;
+
     if(min <10) min = '0' + min;
+    
     if(seg <10) seg= '0' + seg;
 
     horas.textContent = hr
